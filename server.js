2ed51c36fcb1d9d580d73279cfda567070325e43
vxtdx100pre-code <vxtdx100pre@gmail.com>
2025-11-04 22:10:43 -0500

    Limpiar server.js y eliminar código duplicado

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-
-app.post('/recibir-datos', (req, res) => {
-    const data = req.body;
-    console.log('Datos recibidos:', data);
-    // Aquí puedes guardar los datos en una base de datos o un archivo
-    res.sendStatus(200);
-});
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname)); // Servir archivos estáticos
 
-// Servir un archivo HTML si existe (por ejemplo HitCapp.html)
+// Servir el archivo HTML principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'HitCapp.html'));
 });
 
+// Endpoint para recibir datos
 app.post('/recibir-datos', (req, res) => {
   const data = req.body;
   console.log('Datos recibidos:', data);
   res.sendStatus(200);
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
-});
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
